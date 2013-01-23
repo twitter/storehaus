3ac96e75a7332f18fe493d6c219fb4d9608f3723
Sam Ritchie <sritchie@twitter.com>
2013-01-22 19:34:55 -0800

    add empty store.

---
+++ storehaus-core/src/main/scala/com/twitter/storehaus/EmptyStore.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2013 Twitter Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twitter.storehaus
+
+import com.twitter.util.Future
+
+/**
+ * Concrete empty store implementation.
+ * calling - or + on this store is a no-op.
+ */
+class EmptyStore[K, V] extends Store[EmptyStore[K, V], K, V] {
+  val empty = ReadableStore.empty[K,V]
+  override def get(k: K) = empty.get(k)
+  override def multiGet(ks: Set[K]) = empty.multiGet(ks)
+  override def -(k: K) = Future.value(this)
+  override def +(pair: (K,V)) = Future.value(this)
+}
