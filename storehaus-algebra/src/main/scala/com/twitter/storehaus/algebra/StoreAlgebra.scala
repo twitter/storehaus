0750a4d626cb0de5c8a89f9ec5124146b99ff27f
Sam Ritchie <sritchie@twitter.com>
2013-01-23 12:30:34 -0800

    aggregating store.

--- storehaus-algebra/src/main/scala/com/twitter/storehaus/algebra/StoreAlgebra.scala
+++ storehaus-algebra/src/main/scala/com/twitter/storehaus/algebra/StoreAlgebra.scala
@@ -17,17 +17,21 @@
 package com.twitter.storehaus.algebra
 
 import com.twitter.algebird.{ Monoid, Semigroup }
-import com.twitter.storehaus.ReadableStore
+import com.twitter.util.Future
+import com.twitter.storehaus.{ ReadableStore, Store }
 
 /**
  * import StoreAlgebra.enrich to obtain enrichments.
  */
 object StoreAlgebra {
-  implicit def enrich[K, V](store: ReadableStore[K, V]): AlgebraicStore[K, V] =
-    new AlgebraicStore[K, V](store)
+  implicit def enrichReadableStore[K, V](store: ReadableStore[K, V]): AlgebraicReadableStore[K, V] =
+    new AlgebraicReadableStore[K, V](store)
+
+  implicit def enrichStore[StoreType <: Store[StoreType, K, V], K, V](store: StoreType): AlgebraicStore[StoreType, K, V] =
+    new AlgebraicStore[StoreType, K, V](store)
 }
 
-class AlgebraicStore[K, V](store: ReadableStore[K, V]) {
+class AlgebraicReadableStore[K, V](store: ReadableStore[K, V]) {
   import ReadableStoreMonoid.apply
 
   /**
@@ -46,4 +50,35 @@ class AlgebraicStore[K, V](store: ReadableStore[K, V]) {
       override def get(k: K) = store.get(k) map { _ map { Monoid.sum(_) } }
       override def multiGet(ks: Set[K]) = store.multiGet(ks) map { _ mapValues { Monoid.sum(_) } }
     }
+}
+
+class AlgebraicStore[StoreType <: Store[StoreType, K, V], K, V](store: StoreType) {
+  def aggregating(implicit sg: Monoid[V]): AggregatingStore[StoreType, K, V] = new AggregatingStore(store)
+}
+
+/**
+ * Store which aggregates values added with + into the existing value in the store.
+ * If addition ever results in a zero value, the key is deleted from the store.
+ */
+class AggregatingStore[StoreType <: Store[StoreType, K, V], K, V: Monoid](store: StoreType)
+extends Store[AggregatingStore[StoreType, K, V], K, V] {
+  override def get(k: K) = store.get(k)
+  override def multiGet(ks: Set[K]) = store.multiGet(ks)
+
+  override def -(k: K): Future[AggregatingStore[StoreType, K, V]] =
+    (store - k) map { new AggregatingStore(_) }
+
+  override def +(pair: (K,V)): Future[AggregatingStore[StoreType, K, V]] = {
+    val (k, v) = pair
+    store.get(k) flatMap { oldV: Option[V] =>
+      Monoid.nonZeroOption(oldV.map { Monoid.plus(_, v) } getOrElse v)
+        .map { newV => store + (k -> newV) }
+        .getOrElse(store - k)
+        .map { new AggregatingStore(_) }
+    }
+  }
+
+  override def update(k: K)(fn: Option[V] => Option[V]): Future[AggregatingStore[StoreType, K, V]] = {
+    store.update(k)(fn) map { new AggregatingStore(_) }
+  }
 }
