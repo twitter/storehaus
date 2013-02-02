62cdd1f02d36aa724637a1e7fb003f2e789f703f
Sam Ritchie <sritchie@twitter.com>
2013-02-01 18:43:54 -0800

    buffering store

--- storehaus-algebra/src/main/scala/com/twitter/storehaus/algebra/AggregatingStore.scala
+++ storehaus-algebra/src/main/scala/com/twitter/storehaus/algebra/AggregatingStore.scala
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License. You may obtain
-b * a copy of the License at
+ * a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -24,48 +24,31 @@ import com.twitter.storehaus.Store
  * Store which aggregates values added with + into the existing value in the store.
  * If addition ever results in a zero value, the key is deleted from the store.
  *
- * The optional "capacity" parameter allows the store to buffer and aggregate
- * that many kv pairs in an internal SummingQueue before committing them all
- * out after "capacity" total kv pairs are submitted.
- *
  * @author Sam Ritchie
  */
 
 object AggregatingStore {
-  def apply[StoreType <: Store[StoreType, K, V], K, V: Monoid](store: StoreType, capacity: Int = 0) =
-    new AggregatingStore[StoreType, K, V](store, SummingQueue[Map[K, V]](capacity))
+  def apply[StoreType <: Store[StoreType, K, V], K, V: Monoid](store: StoreType) =
+    new AggregatingStore[StoreType, K, V](store)
 }
 
-class AggregatingStore[StoreType <: Store[StoreType, K, V], K, V: Monoid](store: StoreType,
-                                                                          queue: SummingQueue[Map[K, V]])
+class AggregatingStore[StoreType <: Store[StoreType, K, V], K, V: Monoid](store: StoreType)
 extends Store[AggregatingStore[StoreType, K, V], K, V] {
-  override def get(k: K) = submit(queue.flush).flatMap { _.get(k) }
-  override def multiGet(ks: Set[K]) = submit(queue.flush).flatMap { _.multiGet(ks) }
+  override def get(k: K) = store.get(k)
+  override def multiGet(ks: Set[K]) = store.multiGet(ks)
 
-  protected def update(s: StoreType, pair: (K, V)): Future[StoreType] = {
+  override def -(k: K): Future[AggregatingStore[StoreType, K, V]] =
+    (store - k) map { new AggregatingStore(_) }
+
+  override def +(pair: (K,V)): Future[AggregatingStore[StoreType, K, V]] = {
     val (k, v) = pair
     store.get(k) flatMap { oldV: Option[V] =>
       Monoid.nonZeroOption(oldV.map { Monoid.plus(_, v) } getOrElse v)
         .map { newV => store + (k -> newV) }
         .getOrElse(store - k)
+        .map { new AggregatingStore(_) }
     }
   }
-
-  protected def submit(optM: Option[Map[K, V]]): Future[StoreType] = {
-    val storeFuture = Future.value(store)
-    optM.map { _.foldLeft(storeFuture) { (s, pair) => s.flatMap { update(_, pair) } } }
-      .getOrElse(storeFuture)
-  }
-
-  override def -(k: K): Future[AggregatingStore[StoreType, K, V]] =
-    submit(queue.flush)
-      .flatMap { s => (s - k).map { new AggregatingStore(_, queue) } }
-
-  override def +(pair: (K, V)): Future[AggregatingStore[StoreType, K, V]] =
-    submit(queue(Map(pair)))
-      .map { new AggregatingStore(_, queue) }
-
   override def update(k: K)(fn: Option[V] => Option[V]): Future[AggregatingStore[StoreType, K, V]] =
-    submit(queue.flush)
-      .flatMap { _.update(k)(fn).map { new AggregatingStore(_, queue) } }
+    store.update(k)(fn) map { new AggregatingStore(_) }
 }
