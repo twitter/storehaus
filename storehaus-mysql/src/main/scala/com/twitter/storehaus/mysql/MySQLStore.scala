9fa4ecae53e17501e08f8c8a8608cfd3df3ef648
Ruban Monu <rmonu@twitter.com>
2013-04-04 11:53:08 -0700

    First impl

--- storehaus-mysql/src/main/scala/com/twitter/storehaus/mysql/MySQLStore.scala
+++ storehaus-mysql/src/main/scala/com/twitter/storehaus/mysql/MySQLStore.scala
@@ -16,39 +16,86 @@
 
 package com.twitter.storehaus.mysql
 
-import com.twitter.finagle.exp.mysql.Client
+import com.twitter.finagle.exp.mysql.{ Client, Result, StringValue }
 import com.twitter.storehaus.Store
 import com.twitter.util.Future
+
 import org.jboss.netty.buffer.ChannelBuffer
+import org.jboss.netty.buffer.ChannelBuffers
+import org.jboss.netty.util.CharsetUtil.UTF_8 
 
 object MySQLStore {
 
-  def apply(client: Client) = new MySQLStore(client)
+  def apply(client: Client, table: String, kCol: String, vCol: String) = 
+    new MySQLStore(client, table, kCol, vCol)
 }
 
-class MySQLStore(client: Client) extends Store[String, ChannelBuffer] {
+class MySQLStore(client: Client, table: String, kCol: String, vCol: String)
+    extends Store[String, ChannelBuffer] {
 
   override def get(k: String): Future[Option[ChannelBuffer]] = {
-    Future[Option[ChannelBuffer]](Option.empty)
-    // TODO: generate and execute sql select query for the input key
     // finagle-mysql select() method lets you pass in a mapping function
     // to convert resultset into desired output format (ChannelBuffer in this case)
-    // we assume the mysql client already has the dbname/schema selected
+    // we assume here the mysql client already has the dbname/schema selected
+    val selectSql = "SELECT " + g(vCol) + " FROM " + g(table) + " WHERE " + g(kCol) + "=" + q(k)
+    val mysqlResult: Future[Seq[ChannelBuffer]] = client.select(selectSql) { row =>
+      val StringValue(v) = row(vCol).get
+      ChannelBuffers.copiedBuffer(v, UTF_8)
+    }
+    Future.value(mysqlResult.get.lift(0))
   }
 
   override def multiGet[K1 <: String](ks: Set[K1]): Map[K1, Future[Option[ChannelBuffer]]] = {
-    Map[K1, Future[Option[ChannelBuffer]]]()
-    // TODO: generate and execute sql select query for the input keys
+    val keyList = ks.map(q(_)).mkString(",")
+    val selectSql = "SELECT " + g(kCol) + ", " + g(vCol) + " FROM " + g(table) + " WHERE " + g(kCol) + " IN (" + keyList + ")"
+    val mysqlResult: Future[Seq[(String, ChannelBuffer)]] = client.select(selectSql) { row =>
+      val StringValue(k) = row(kCol).get
+      val StringValue(v) = row(vCol).get
+      (k, ChannelBuffers.copiedBuffer(v, UTF_8))
+    }
+    val storehausResult = collection.mutable.Map.empty[K1, Future[Option[ChannelBuffer]]]
+    for ( row <- mysqlResult.get ) {
+      storehausResult += row._1.asInstanceOf[K1] -> Future.value(Some(row._2))
+    }
+    for ( key <- ks ) {
+      if (!storehausResult.contains(key)) {
+        storehausResult += key -> Future[Option[ChannelBuffer]](Option.empty)
+      }
+    }
+    storehausResult.toMap
   }
 
-  protected def set(k: String, v: ChannelBuffer) = {
-    // TODO: generate and execute dml query for the input key-value pair
-  }
+  protected def set(k: String, v: ChannelBuffer) = doSet(k, v).get
 
   override def put(kv: (String, Option[ChannelBuffer])): Future[Unit] = {
-    Future[Unit]()
-    // TODO: async version of set
+    kv match {
+      case (key, Some(value)) => doSet(key, value).unit
+      case (key, None) => doDelete(key).unit
+    }
+  }
+  
+  override def close { client.close }
+
+  protected def doSet(k: String, v: ChannelBuffer): Future[Result] = {
+    // mysql's insert-or-update syntax works only when a primary key is defined:
+    // http://dev.mysql.com/doc/refman/5.1/en/insert-on-duplicate.html
+    // since we are not guaranteed that, we first check if key exists
+    // and insert or update accordingly
+    val getResult = get(k).get
+    val setSql = if (getResult.isEmpty) {
+        "INSERT INTO " + g(table) + "(" + g(kCol) + "," + g(vCol) + ")" + " VALUES (" + q(k) + "," + q(v.toString(UTF_8)) +")"
+      } else {
+        "UPDATE " + g(table) + " SET " + g(vCol) + "=" + q(v.toString(UTF_8)) + " WHERE " + g(kCol) + "=" + q(k)
+      }
+    client.query(setSql)
   }
 
-  override def close { client.close }
+  protected def doDelete(k: String): Future[Result] = {
+    val deleteSql = "DELETE FROM " + g(table) + " WHERE " + g(kCol) + "=" + q(k)
+    client.query(deleteSql)
+  }
+
+  protected def q(s: String)  = "'" + s + "'"
+
+  protected def g(s: String)  = "`" + s + "`"
 }
