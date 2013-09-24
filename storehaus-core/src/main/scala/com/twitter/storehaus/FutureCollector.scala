82bc2c5813e73fb042de266bcdb30b42b6bc88c6
P. Oscar Boykin <oscar@twitter.com>
2013-09-24 16:56:19 -0700

    Don't catch all, we could be eating Errors

--- storehaus-core/src/main/scala/com/twitter/storehaus/FutureCollector.scala
+++ storehaus-core/src/main/scala/com/twitter/storehaus/FutureCollector.scala
@@ -41,7 +41,7 @@ object FutureCollector {
     override def apply(futureSeq: Seq[Future[T]]) =
       Future.collect {
         futureSeq.map { f: Future[T] =>
-          f.map { Some(_) }.handle { case _ => None }
+          f.map { Some(_) }.handle { case x: Exception => None } //ignore exceptions, not Errors!
         }
       }.map { _.flatten }
   }
