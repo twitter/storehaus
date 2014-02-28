5a5b7ee6f4b85619a3addb0d430be2fbda8dbee1
Ruban Monu <rmonu@twitter.com>
2014-02-27 18:54:53 -0800

    improvements

--- storehaus-core/src/main/scala/com/twitter/storehaus/ReadThroughStore.scala
+++ storehaus-core/src/main/scala/com/twitter/storehaus/ReadThroughStore.scala
@@ -16,43 +16,76 @@
 
 package com.twitter.storehaus
 
+import com.twitter.concurrent.AsyncMutex
 import com.twitter.util.Future
 
 /**
  * Provides read-through caching on a readable store fronted by a cache.
+ *
+ * Keys are fetched from backing store on cache miss and cache read failures.
+ *
+ * All cache operations are best effort i.e. 'get' will return the key from
+ * backing store even if adding/updating the cached copy fails.
+ *
+ * On the other hand, any failure while reading from backing store
+ * is propagated to the client.
+ *
+ * Thread-safety is achieved using a mutex.
  *
  * @author Ruban Monu
  */
 class ReadThroughStore[K, V](backingStore: ReadableStore[K, V], cache: Store[K, V])
   extends ReadableStore[K, V] {
 
-  override def get(k: K): Future[Option[V]] = {
+  protected [this] lazy val mutex = new AsyncMutex
+
+  private [this] def getFromBackingStore(k: K) : Future[Option[V]] =
+    // attempt to fetch the key from backing store and
+    // write the key to cache, best effort
+    backingStore.get(k).flatMap { storeValue =>
+      cache.put((k, storeValue))
+        .map { u : Unit => storeValue }
+        .onFailure { case x: Exception => storeValue }
+    }
+
+  override def get(k: K): Future[Option[V]] = mutex.acquire.flatMap { p =>
     cache.get(k).flatMap { cacheValue =>
       cacheValue match {
-        case None => backingStore.get(k).flatMap { storeValue =>
-          cache.put((k, storeValue)).map { u : Unit => storeValue }
-        }
-        case _ => Future.value(cacheValue)
+        case None => getFromBackingStore(k)
+        case some => Future.value(some)
       }
+    } onFailure { case x: Exception =>
+      getFromBackingStore(k)
+    } ensure {
+      p.release
     }
   }
 
   override def multiGet[K1 <: K](ks: Set[K1]): Map[K1, Future[Option[V]]] = {
-    val result: Future[Map[K1, Option[V]]] =
-      FutureOps.mapCollect(cache.multiGet(ks)).flatMap { cacheResult =>
-        val hits = cacheResult.filter { !_._2.isEmpty }
-        val missedKeys = cacheResult.filter { _._2.isEmpty }.keySet
-        FutureOps.mapCollect(backingStore.multiGet(missedKeys)).flatMap { storeResult =>
-          FutureOps.mapCollect(cache.multiPut(storeResult)).map { u =>
-            hits ++ storeResult
-          }
-          // mapCollect will fail the future if any keys fail to write to cache.
-          // TODO: if cache write fails for some keys in the batch,
-          // should we invalidate other keys in this batch?
-          // or should we make the cache writes best effort?
+    val f : Future[Map[K1, Option[V]]] = mutex.acquire.flatMap { p =>
+      // attempt to read from cache first
+      val cacheResults : Map[K1, Future[Either[Option[V], Exception]]] =
+        cache.multiGet(ks).map { case (k, f) =>
+          (k, f.map { optv => Left(optv) } onFailure { case x: Exception => Right(x) })
         }
+
+      // attempt to read all failed keys and cache misses from backing store
+      FutureOps.mapCollect(cacheResults).flatMap { cacheResult =>
+        val failedKeys = cacheResult.filter { _._2.isRight }.keySet
+        val responses = cacheResult.filter { _._2.isLeft }.map { case (k, r) => (k, r.left.get) }
+        val hits = responses.filter { !_._2.isEmpty }
+        val missedKeys = responses.filter { _._2.isEmpty }.keySet
+
+        FutureOps.mapCollect(backingStore.multiGet(missedKeys ++ failedKeys)).flatMap { storeResult =>
+          // write fetched keys to cache, best effort
+          FutureOps.mapCollect(cache.multiPut(storeResult))(FutureCollector.bestEffort[(K1, Unit)])
+            .map { u => hits ++ storeResult }
+        }
+      } ensure {
+        p.release
       }
-    FutureOps.liftValues(ks, result, { (k: K1) => Future.None })
+    }
+    FutureOps.liftValues(ks, f, { (k: K1) => Future.None })
   }
 }
 
