ff96d5ee206c782cfa9f1df8e486962b8407df83
Ian O Connell <ioconnell@twitter.com>
2014-03-20 11:00:04 -0700

    Release 0.9.0

--- project/Build.scala
+++ project/Build.scala
@@ -55,7 +55,7 @@ object StorehausBuild extends Build {
   val sharedSettings = extraSettings ++ ciSettings ++ Seq(
     organization := "com.twitter",
     scalaVersion := "2.9.3",
-    version := "0.8.0",
+    version := "0.9.0",
     crossScalaVersions := Seq("2.9.3", "2.10.0"),
     javacOptions ++= Seq("-source", "1.6", "-target", "1.6"),
     javacOptions in doc := Seq("-source", "1.6"),
@@ -114,12 +114,12 @@ object StorehausBuild extends Build {
   def youngestForwardCompatible(subProj: String) =
     Some(subProj)
       .filterNot(unreleasedModules.contains(_))
-      .map { s => "com.twitter" % ("storehaus-" + s + "_2.9.3") % "0.8.0" }
+      .map { s => "com.twitter" % ("storehaus-" + s + "_2.9.3") % "0.9.0" }
 
   val algebirdVersion = "0.5.0"
   val bijectionVersion = "0.6.2"
   val utilVersion = "6.11.0"
-  val scaldingVersion = "0.9.0rc4"
+  val scaldingVersion = "0.9.0rc15"
 
   lazy val storehaus = Project(
     id = "storehaus",
