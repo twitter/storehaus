027b5272666306abb8b5bd12217cfa9c5e909e04
Ian O Connell <ioconnell@twitter.com>
2014-06-04 13:40:59 -0700

    Remove commented code

--- storehaus-caliper/src/main/scala/com/twitter/storehaus/caliper/Runner.scala
+++ storehaus-caliper/src/main/scala/com/twitter/storehaus/caliper/Runner.scala
@@ -11,17 +11,4 @@ object Runner {
     CaliperMain.main(args)
   }
 
-  // def main(args: Array[String]) {
-  //   val th = if(args.size > 0 && args(0) == "warmed") com.ichi.bench.Thyme.warmed(verbose = print) else new com.ichi.bench.Thyme
-  //   val fn = new WriteThroughCacheBenchmark
-  //   fn.numInputKeys = 500
-  //   fn.numElements = 100000
-  //   println("Starting set up")
-  //   fn.setUp
-  //   println("Starting benchmark")
-  //   println(th.pbench(fn.timeDoUpdates(10)))
-  //   println("Finished benchmark")
-  //   System.exit(1)
-  // }
-
 }
