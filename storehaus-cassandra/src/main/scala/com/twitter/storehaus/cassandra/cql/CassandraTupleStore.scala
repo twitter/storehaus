4ca2b6ea02b3478340442e0df2d34c8c11297ab9
Andreas Petter <andreas.petter@gmail.com>
2014-08-25 13:04:40 +0200

    Tuple-store

---
+++ storehaus-cassandra/src/main/scala/com/twitter/storehaus/cassandra/cql/CassandraTupleStore.scala
@@ -0,0 +1,29 @@
+package com.twitter.storehaus.cassandra.cql
+
+import com.twitter.storehaus.Store
+import com.twitter.util.Future
+import shapeless.Tuples._
+import shapeless._
+
+/**
+ * converter store for Tuples to HList. This makes it easy to use this store
+ * with tuples, which can in turn be serialized easily.
+ */
+class CassandraTupleStore[RKT <: Product, CKT <: Product, V, RK <: HList, CK <: HList, RS <: HList, CS <: HList]
+		(store: AbstractCQLCassandraCompositeStore[RK, CK, V, RS, CS])
+		(implicit ev1: HListerAux[RKT, RK],
+		    ev2: HListerAux[CKT, CK],
+		    ev3: TuplerAux[RK, RKT],
+		    ev4: TuplerAux[CK, CKT])
+	extends Store[(RKT, CKT), V] {
+
+  override def get(k: (RKT, CKT)): Future[Option[V]] = store.get((k._1.hlisted, k._2.hlisted))
+  
+  override def multiPut[K1 <: (RKT, CKT)](kvs: Map[K1, Option[V]]): Map[K1, Future[Unit]] = {
+    val internalMapFunction = (k: (RKT, CKT), v : Option[V]) => {
+      ((k._1.hlisted, k._2.hlisted), v)
+    }
+    val resultMap = store.multiPut(kvs.map(kv =>  internalMapFunction(kv._1, kv._2)))
+    resultMap.map(kv => ((kv._1._1.tupled, kv._1._2.tupled).asInstanceOf[K1], kv._2))
+  }
+}
