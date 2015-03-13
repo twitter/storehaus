74798674fa4162399cb4b4f1bc58ffe3ee6d1e3f
BenFradet <benjamin.fradet@gmail.com>
2015-03-13 17:59:30 +0100

    added byte arrays generators to NonEmpty and commented

--- storehaus-testing/src/main/scala/com/twitter/storehaus/testing/generator/NonEmpty.scala
+++ storehaus-testing/src/main/scala/com/twitter/storehaus/testing/generator/NonEmpty.scala
@@ -22,11 +22,19 @@ import org.scalacheck.{ Choose, Gen }
 object NonEmpty {
   /** Generator for non-empty alpha strings of random length */
   def alphaStr: Gen[String] =
-    for(cs <- Gen.listOf1(Gen.alphaChar)) yield cs.mkString
+    for (cs <- Gen.listOf1(Gen.alphaChar)) yield cs.mkString
 
   /** Generator for Options of non-empty alpha strings of random length */
   def alphaStrOpt: Gen[Option[String]] =
     alphaStr.flatMap(str => Gen.oneOf(Some(str), None))
+
+  /** Generator for non-empty byte arrays of random length */
+  def byteArray: Gen[Array[Byte]] =
+    for (cs <- Gen.listOf1(Gen.alphaChar)) yield cs.mkString.getBytes
+
+  /** Generator for Options of non-empty by arrays of random length */
+  def byteArrayOpt: Gen[Option[Array[Byte]]] =
+    byteArray.flatMap(array => Gen.oneOf(Some(array), None))
 
   /** Storehaus pairings of non-empty data.
    *  In most cases this means 2 element tuples of(K, Option[V]) */
@@ -37,12 +45,20 @@ object NonEmpty {
       opt <- NonEmpty.alphaStrOpt
     } yield (str, opt)
 
+    /** Generator for pairings of non-empty byte arrays and non-empty byte
+      arrays options*/
+    def byteArrayPair: Gen[(Array[Byte], Option[Array[Byte]])] = for {
+      array <- NonEmpty.byteArray
+      opt <- NonEmpty.byteArrayOpt
+    } yield (array, opt)
+
     /** Generator for pairings of non-empty alpha strings to options of positive numerics */
     def alphaStrPosNumericPair[T : Numeric : Choose]: Gen[(String, Option[T])] = for {
       str <- NonEmpty.alphaStr
       opt <- Gen.posNum[T].flatMap(l => Gen.oneOf(Some(l), None))
     } yield (str, opt)
 
+    /** Generator for pairings of numerics */
     def numericPair[T : Numeric : Choose]: Gen[(T, Option[T])] = for {
       num <- Gen.posNum[T]
       opt <- Gen.posNum[T].flatMap(l => Gen.oneOf(Some(l), None))
@@ -51,6 +67,10 @@ object NonEmpty {
     /** Generator for non-empty lists of (String, Option[String])'s */
     def alphaStrs(n: Int = 10) =
       Gen.listOfN(n, alphaStrPair)
+
+    /** Generator for non-empty lists of (Array[Byte], Option[Array[Byte]])'s */
+    def byteArrays(n: Int = 10) =
+      Gen.listOfN(n, byteArrayPair)
 
     /** Generator for non-empty lists of (String, Option[T])'s */
     def alphaStrNumerics[T : Numeric : Choose](n: Int = 10) =
