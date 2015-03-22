d0223969e7ae069ef9ff06f6de332f008e58f1e7
BenFradet <benjamin.fradet@gmail.com>
2015-03-22 20:20:31 +0100

    test for multiput in LevelDBStore

--- storehaus-leveldb/src/test/scala/com/twitter/storehaus/leveldb/LevelDBStoreProperties.scala
+++ storehaus-leveldb/src/test/scala/com/twitter/storehaus/leveldb/LevelDBStoreProperties.scala
@@ -5,7 +5,7 @@ import java.util
 
 import com.twitter.storehaus.Store
 import com.twitter.storehaus.testing.generator.NonEmpty
-import com.twitter.util.Await
+import com.twitter.util.{Future, Await}
 import org.iq80.leveldb.Options
 import org.scalacheck.{Gen, Properties}
 import org.scalacheck.Prop.forAll
@@ -17,7 +17,7 @@ import scala.util.Random
  */
 object LevelDBStoreProperties extends Properties("LevelDBStore") {
 
-  def putAndGetStoreTest(store: Store[Array[Byte], Array[Byte]],
+  def putAndGetTest(store: Store[Array[Byte], Array[Byte]],
                          pairs: Gen[List[(Array[Byte], Option[Array[Byte]])]]) =
     forAll(pairs) { examples: List[(Array[Byte], Option[Array[Byte]])] =>
       examples.forall {
@@ -32,11 +32,35 @@ object LevelDBStoreProperties extends Properties("LevelDBStore") {
       }
     }
 
-  property("LevelDB[Array[Byte], Array[Byte]]") = {
+  def multiPutAndGetTest(store: Store[Array[Byte], Array[Byte]],
+                         pairs: Gen[List[(Array[Byte], Option[Array[Byte]])]]) =
+    forAll(pairs) { examples: List[(Array[Byte], Option[Array[Byte]])] =>
+      val examplesMap = examples.toMap
+      Await.result(Future.collect(store.multiPut(examplesMap).values.toList))
+      examples.forall {
+        case (k, v) => {
+          val found = Await.result(store.get(k))
+          found match {
+            case Some(a) => util.Arrays.equals(a, v.get)
+            case None => found == examplesMap(k)
+          }
+        }
+      }
+    }
+
+  property("LevelDB[Array[Byte], Array[Byte]] single") = {
     val dir = new File(System.getProperty("java.io.tmpdir"),
       "leveldb-test-" + new Random().nextInt(Int.MaxValue))
     dir.mkdirs()
-    val store = new LevelDBStore(dir, new Options().createIfMissing(true), 2)
-    putAndGetStoreTest(store, NonEmpty.Pairing.byteArrays())
+    val store = new LevelDBStore(dir, new Options(), 2)
+    putAndGetTest(store, NonEmpty.Pairing.byteArrays())
+  }
+
+  property("LevelDB[Array[Byte], Array[Byte] multi") = {
+    val dir = new File(System.getProperty("java.io.tmpdir"),
+      "leveldb-test-multi-" + new Random().nextInt(Int.MaxValue))
+    dir.mkdirs()
+    val store = new LevelDBStore(dir, new Options(), 2)
+    multiPutAndGetTest(store, NonEmpty.Pairing.byteArrays())
   }
 }
