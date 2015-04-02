1c40b43564c33ce93a0acf0a777c6ed3f8bc8e3c
Christian Zirpins <c.zirpins@seeburger.de>
2015-04-02 13:07:49 +0200

    pull up ThrottlingConfig

--- storehaus-cassandra/src/main/scala/com/twitter/storehaus/cassandra/cql/cascading/CassandraSplittingMechanism.scala
+++ storehaus-cassandra/src/main/scala/com/twitter/storehaus/cassandra/cql/cascading/CassandraSplittingMechanism.scala
@@ -21,7 +21,7 @@ import org.apache.cassandra.thrift.Cassandra
 import org.apache.thrift.protocol.TBinaryProtocol
 import org.apache.cassandra.thrift.TFramedTransportFactory
 import org.apache.cassandra.hadoop.{ AbstractColumnFamilyInputFormat, ColumnFamilySplit, ConfigHelper }
-import com.twitter.util.{Throw, Try}
+import com.twitter.util.{ Throw, Try }
 import com.twitter.storehaus.cascading.Instance
 import com.twitter.storehaus.cascading.split.StorehausSplittingMechanism
 import org.apache.cassandra.hadoop.cql3.{ CqlRecordReader, CqlInputFormat, CqlConfigHelper }
@@ -34,31 +34,31 @@ import java.net.InetAddress
 import org.slf4j.{ Logger, LoggerFactory }
 import java.io.IOException
 
-
 /**
- * Cassandra does not directly support Splitting, so we provide 
+ * Cassandra does not directly support Splitting, so we provide
  * a special splitting mechanism which is able to split a Cassandra
  */
-class CassandraSplittingMechanism[K, V, U <: CassandraCascadingInitializer[K, V]](override val conf: JobConf) 
-	extends StorehausSplittingMechanism[K, V, U](conf: JobConf) {
+class CassandraSplittingMechanism[K, V, U <: CassandraCascadingInitializer[K, V]](override val conf: JobConf)
+  extends StorehausSplittingMechanism[K, V, U](conf: JobConf) {
   @transient private val log = LoggerFactory.getLogger(classOf[CassandraSplittingMechanism[K, V, U]])
-  
+
   val SPLIT_RETRIES = "com.twitter.storehaus.cassandra.cql.cascading.splitretries"
-  
+
   val tapid = InitializableStoreObjectSerializer.getTapId(conf)
   val readVersion = InitializableStoreObjectSerializer.getReadVerion(conf, tapid)
   val storeinit = readVersion match {
-    case None => InitializableStoreObjectSerializer.getReadableStoreIntializer(conf, tapid).get.asInstanceOf[CassandraCascadingInitializer[K, V]]
+    case None          => InitializableStoreObjectSerializer.getReadableStoreIntializer(conf, tapid).get.asInstanceOf[CassandraCascadingInitializer[K, V]]
     case Some(version) => InitializableStoreObjectSerializer.getReadableVersionedStoreIntializer(conf, tapid, version).get.asInstanceOf[CassandraCascadingInitializer[K, V]]
-  } 
-  lazy val rowMatcher = storeinit.getCascadingRowMatcher  
-  
-  override def getSplits(job: JobConf, hint: Int) : Array[InputSplit] = {
+  }
+  lazy val rowMatcher = storeinit.getCascadingRowMatcher
+
+  override def getSplits(job: JobConf, hint: Int): Array[InputSplit] = {
     val retryMaxNumber = Try(Option(conf.get(SPLIT_RETRIES)).map(_.toInt)).toOption.flatten.getOrElse(10)
     def retrySplittingMechnism(tryNumber: Int, columnFamilyFormat: CqlInputFormat): Try[Array[CassandraStorehausSplit]] = {
-      Try(columnFamilyFormat.getSplits(conf, hint).map(split => 
+      Try(columnFamilyFormat.getSplits(conf, hint).map(split =>
         CassandraStorehausSplit(tapid, split.asInstanceOf[ColumnFamilySplit]))).
-        onFailure { e => if(tryNumber < retryMaxNumber) {
+        onFailure { e =>
+          if (tryNumber < retryMaxNumber) {
             log.warn(s"Retrying getting splits due to error:", e)
             retrySplittingMechnism(tryNumber + 1, columnFamilyFormat)
           } else {
@@ -67,7 +67,7 @@ class CassandraSplittingMechanism[K, V, U <: CassandraCascadingInitializer[K, V]
         }
     }
     // ask for contact information -> call get_splits_ex via ColumnFamilyInputFormat
-    log.debug(s"Getting splits for StorehausTap with id $tapid from Cassandra") 
+    log.debug(s"Getting splits for StorehausTap with id $tapid from Cassandra")
     val connectionOptions = storeinit.getThriftConnections.trim.split(",")(0).split(":").map(s => s.trim)
     ConfigHelper.setInputInitialAddress(conf, connectionOptions.head)
     ConfigHelper.setInputRpcPort(conf, if (connectionOptions.size == 1) "9160" else connectionOptions.last)
@@ -76,13 +76,13 @@ class CassandraSplittingMechanism[K, V, U <: CassandraCascadingInitializer[K, V]
     CqlConfigHelper.setInputColumns(conf, storeinit.getCascadingRowMatcher.getColumnNamesString)
     CqlConfigHelper.setInputNativePort(conf, storeinit.getNativePort.toString)
     val columnFamilyFormat = new CqlInputFormat
-    val splits = retrySplittingMechnism(0, columnFamilyFormat).onFailure { e => 
-        log.error(s"Got Exception from Cassandra while getting splits on seeds ${ ConfigHelper.getInputInitialAddress(conf)}", e)
-        throw e
+    val splits = retrySplittingMechnism(0, columnFamilyFormat).onFailure { e =>
+      log.error(s"Got Exception from Cassandra while getting splits on seeds ${ConfigHelper.getInputInitialAddress(conf)}", e)
+      throw e
     }
     splits.get.toArray
   }
-  
+
   override def initializeSplitInCluster(split: InputSplit, reporter: Reporter): Unit = {
     val storesplit = getStorehausSplit(split)
     // if desired, throttle speed of how fast mappers are created
@@ -101,29 +101,29 @@ class CassandraSplittingMechanism[K, V, U <: CassandraCascadingInitializer[K, V]
     // CqlConfigHelper.setInputCql(conf, storeinit.getUserDefinedWhereClauses(readVersion))
     storesplit.recordReader = new CqlRecordReader
     val tac = new TaskAttemptContext(conf, TaskAttemptID.forName(conf.get(AbstractColumnFamilyInputFormat.MAPRED_TASK_ID))) {
-      override def progress() = reporter.progress()        
+      override def progress() = reporter.progress()
     }
-    log.debug(s"Initializing RecordReader for StorehausTap with id $tapid") 
+    log.debug(s"Initializing RecordReader for StorehausTap with id $tapid")
     storesplit.recordReader.initialize(split.asInstanceOf[org.apache.hadoop.mapreduce.InputSplit], tac)
   }
-  
+
   /**
    * similar to InputSplit.next
    */
   override def fillRecord(split: InputSplit, key: Instance[K], value: Instance[V]): Boolean = {
     val storesplit = getStorehausSplit(split)
-    if(storesplit.recordReader.nextKeyValue) {
+    if (storesplit.recordReader.nextKeyValue) {
       val row = storesplit.recordReader.getCurrentValue
       val (cassKey, cassValue) = rowMatcher.getKeyValueFromRow(row)
-      log.debug(s"Filling record for StorehausTap with id $tapid with value=$cassValue and key=$cassKey") 
+      log.debug(s"Filling record for StorehausTap with id $tapid with value=$cassValue and key=$cassKey")
       key.set(cassKey)
       value.set(cassValue)
       true
     } else false
   }
-  
+
   private def getStorehausSplit(split: InputSplit): CassandraStorehausSplit = split.asInstanceOf[CassandraStorehausSplit]
-  
+
   /**
    * free resources after splitting is done
    */
@@ -134,20 +134,23 @@ class CassandraSplittingMechanism[K, V, U <: CassandraCascadingInitializer[K, V]
    */
   override def closeSplit(split: InputSplit) = getStorehausSplit(split).recordReader.close()
 
-  object ThrottlingConfig { 
-    val MAPPING_THROTTLING_SECONDS = "com.twitter.storehaus.cassandra.cql.cascading.mapthrottlesecs"
+}
+
+object ThrottlingConfig {
+    @transient private val log = LoggerFactory.getLogger("ThrottlingConfig")
   
-    def waitConfiguredSeconds(conf: JobConf): Unit = Option(conf.get(MAPPING_THROTTLING_SECONDS)) match {
-        case Some(seconds) => Try {
-          val secs = seconds.toInt
-          log.info(s"Mapper is waiting configured $secs seconds to throttle query speed.")
-          Thread.sleep(secs * 1000L)
-        }.onFailure ( _ match {
-          case n: NumberFormatException => log.error(s"$seconds does not seem to be an int described by $MAPPING_THROTTLING_SECONDS", n)
-          case e: Exception => log.error(s"Exception on waiting for $seconds as configured for $MAPPING_THROTTLING_SECONDS", e)
-        })
-        case None => 
-      }
+  val MAPPING_THROTTLING_SECONDS = "com.twitter.storehaus.cassandra.cql.cascading.mapthrottlesecs"
+
+  def waitConfiguredSeconds(conf: JobConf): Unit = Option(conf.get(MAPPING_THROTTLING_SECONDS)) match {
+    case Some(seconds) => Try {
+      val secs = seconds.toInt
+      log.info(s"Mapper is waiting configured $secs seconds to throttle query speed.")
+      Thread.sleep(secs * 1000L)
+    }.onFailure(_ match {
+      case n: NumberFormatException => log.error(s"$seconds does not seem to be an int described by $MAPPING_THROTTLING_SECONDS", n)
+      case e: Exception             => log.error(s"Exception on waiting for $seconds as configured for $MAPPING_THROTTLING_SECONDS", e)
+    })
+    case None =>
   }
 }
 
