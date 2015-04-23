f90f0426b46b3026053459c2ad11ca7c3bea1e75
BenFradet <benjamin.fradet@gmail.com>
2015-04-23 20:07:21 +0200

    test of batch writing for leveldb

--- storehaus-leveldb/src/test/scala/com/twitter/storehaus/leveldb/LevelDBStoreProperties.scala
+++ storehaus-leveldb/src/test/scala/com/twitter/storehaus/leveldb/LevelDBStoreProperties.scala
@@ -37,16 +37,23 @@ object LevelDBStoreProperties extends Properties("LevelDBStore") {
     forAll(pairs) { examples: List[(Array[Byte], Option[Array[Byte]])] =>
       val examplesMap = examples.toMap
       Await.result(Future.collect(store.multiPut(examplesMap).values.toList))
-      examples.forall {
-        case (k, v) => {
-          val found = Await.result(store.get(k))
-          found match {
-            case Some(a) => util.Arrays.equals(a, v.get)
-            case None => found == examplesMap(k)
-          }
-        }
-      }
+      val result = store.multiGet(examplesMap.keySet)
+
+      val stringifiedResults = stringifyMap(result.map(kv => kv match {
+        case (key, v) => (key, Await.result(v))
+      }))
+      val stringifiedExamples = stringifyMap(examplesMap)
+
+      stringifiedResults == stringifiedExamples
     }
+
+  private def stringifyMap(map: Map[Array[Byte], Option[Array[Byte]]])
+  :Map[String, Option[String]] = {
+    map.map(kv => kv match {
+      case (k, Some(v)) => (new String(k), Some(new String(v)))
+      case (k, None) => (new String(k), None)
+    })
+  }
 
   property("LevelDB[Array[Byte], Array[Byte]] single") = {
     val dir = new File(System.getProperty("java.io.tmpdir"),
@@ -60,7 +67,7 @@ object LevelDBStoreProperties extends Properties("LevelDBStore") {
     val dir = new File(System.getProperty("java.io.tmpdir"),
       "leveldb-test-multi-" + new Random().nextInt(Int.MaxValue))
     dir.mkdirs()
-    val store = new LevelDBStore(dir, new Options(), 2)
+    val store = new LevelDBStore(dir, new Options(), 1)
     multiPutAndGetTest(store, NonEmpty.Pairing.byteArrays())
   }
 }
