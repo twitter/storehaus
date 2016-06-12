e2857ca02f5c932e63e84dffc252be982f13a960
BenFradet <benjamin.fradet@gmail.com>
2016-06-12 17:20:29 +0200

    cleanup of DefaultHBaseCluster

--- storehaus-hbase/src/test/scala/com/twitter/storehaus/hbase/DefaultHBaseCluster.scala
+++ storehaus-hbase/src/test/scala/com/twitter/storehaus/hbase/DefaultHBaseCluster.scala
@@ -27,11 +27,14 @@ import com.twitter.util.Closable
  */
 trait DefaultHBaseCluster[C <: Closable] extends CloseableCleanup[C] {
   val quorumNames = Seq("localhost:2181")
-  val table = "summing_bird"
-  val columnFamily = "sb"
-  val column = "aggregate"
+  val columnFamily = "columnFamily"
+  val column = "column"
   val createTable = true
-  val testingUtil = new HBaseTestingUtility()
+  val testingUtil = {
+    val tu = new HBaseTestingUtility()
+    tu.startMiniCluster()
+    tu
+  }
   val conf = testingUtil.getConfiguration
   val pool = new HTablePool(conf, 1)
 
