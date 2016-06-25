88d2d38b34197dd18014d64dfed8b081402214a4
BenFradet <benjamin.fradet@gmail.com>
2016-06-25 22:14:53 +0200

    cleanup storehaus-caliper

--- storehaus-caliper/src/main/scala/com/twitter/storehaus/caliper/WriteThroughCacheBenchmark.scala
+++ storehaus-caliper/src/main/scala/com/twitter/storehaus/caliper/WriteThroughCacheBenchmark.scala
@@ -6,12 +6,13 @@ import com.twitter.bijection._
 import com.twitter.storehaus._
 import com.twitter.conversions.time._
 import com.twitter.algebird._
-import java.nio.ByteBuffer
+import com.twitter.storehaus.cache.{RollOverFrequencyMS, WriteOperationUpdateFrequency,
+  HeavyHittersPercent, MutableCache}
 import scala.math.pow
 import com.twitter.storehaus.algebra._
-import com.twitter.util.{Await, Future}
+import com.twitter.util.{Duration, Timer, Future}
 
-class DelayedStore[K, V](val self: Store[K, V])(implicit timer: com.twitter.util.Timer) extends StoreProxy[K, V] {
+class DelayedStore[K, V](val self: Store[K, V])(implicit timer: Timer) extends StoreProxy[K, V] {
   override def put(kv: (K, Option[V])): Future[Unit] = {
     self.put(kv).delayed(10.milliseconds)
   }
@@ -21,12 +22,12 @@ class DelayedStore[K, V](val self: Store[K, V])(implicit timer: com.twitter.util
   }
 
   override def multiGet[K1 <: K](ks: Set[K1]): Map[K1, Future[Option[V]]] =
-    self.multiGet(ks).map { case (k,v) =>
+    self.multiGet(ks).map { case (k, v) =>
       (k, v.delayed(10.milliseconds))
     }
 
   override def multiPut[K1 <: K](kvs: Map[K1, Option[V]]): Map[K1, Future[Unit]] =
-    self.multiPut(kvs).map { case (k,v) =>
+    self.multiPut(kvs).map { case (k, v) =>
       (k, v.delayed(10.milliseconds))
     }
 }
@@ -49,34 +50,28 @@ class WriteThroughCacheBenchmark extends SimpleBenchmark {
 
   var noCacheStore: MergeableStore[Long, HLL] = _
 
-  override def setUp {
+  override def setUp(): Unit = {
     val rng = new scala.util.Random(3)
     val byteEncoder = implicitly[Injection[Long, Array[Byte]]]
-    def setSize = rng.nextInt(10) + 1 // 1 -> 10
-    def hll(elements: Set[Long]): HLL = hllMonoid.batchCreate(elements)(byteEncoder)
+    def size: Int = rng.nextInt(10) + 1 // 1 -> 10
+    def hll(elements: Set[Long]): HLL =
+      hllMonoid.sum(elements.map(byteEncoder(_)).map(hllMonoid.create))
     val inputIntermediate = (0L until numElements).map {_ =>
-      val setElements = (0 until setSize).map{_ => rng.nextInt(1000).toLong}.toSet
+      val setElements = (0 until size).map{_ => rng.nextInt(1000).toLong}.toSet
       (pow(numInputKeys, rng.nextFloat).toLong, hll(setElements))
     }.grouped(20)
 
     inputData = inputIntermediate.map(s => MapAlgebra.sumByKey(s)).toSeq
 
     val delayedStore = new DelayedStore(new ConcurrentHashMapStore[Long, HLL])
-    store = (new WriteThroughStore(delayedStore, new HHFilteredStore(new ConcurrentHashMapStore[Long, HLL]), true)).toMergeable
+    val hhStore = HHFilteredStore.buildStore[Long, HLL](
+      new ConcurrentHashMapStore[Long, HLL],
+      MutableCache.ttl(Duration.fromSeconds(10000), numElements),
+      HeavyHittersPercent(0.5f),
+      WriteOperationUpdateFrequency(1),
+      RollOverFrequencyMS(10000000L)
+    )
+    store = new WriteThroughStore(delayedStore, hhStore).toMergeable
     noCacheStore = delayedStore.toMergeable
-  }
-
-  def timeDoUpdates(reps: Int): Int = {
-    Await.result(Future.collect((0 until reps).flatMap { indx =>
-      inputData.map(d => FutureOps.mapCollect(store.multiMerge(d)).unit)
-    }.toSeq))
-    12
-  }
-
-  def timeDoUpdatesWithoutCache(reps: Int): Int = {
-    Await.result(Future.collect((0 until reps).flatMap { indx =>
-      inputData.map(d => FutureOps.mapCollect(noCacheStore.multiMerge(d)).unit)
-    }.toSeq))
-    12
   }
 }
