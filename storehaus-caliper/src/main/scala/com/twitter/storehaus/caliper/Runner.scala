248933a5b4745cd29c63fff479d25025cc853d04
BenFradet <benjamin.fradet@gmail.com>
2016-06-25 22:14:53 +0200

    cleanup storehaus-caliper

--- storehaus-caliper/src/main/scala/com/twitter/storehaus/caliper/Runner.scala
+++ storehaus-caliper/src/main/scala/com/twitter/storehaus/caliper/Runner.scala
@@ -1,9 +1,6 @@
 package com.twitter.storehaus.caliper
 
 import com.google.caliper.{Runner => CaliperMain}
-import com.google.common.collect.ObjectArrays.concat
-
-import java.io.PrintWriter
 
 object Runner {
 
