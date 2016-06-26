5ead32a2ffd4fbdb8e6af6618046e06ea74ab838
BenFradet <benjamin.fradet@gmail.com>
2016-06-26 11:04:35 +0200

    cleanup storehaus-kafka

--- storehaus-kafka/src/test/scala/com/twitter/storehaus/kafka/KafkaSinkSpec.scala
+++ storehaus-kafka/src/test/scala/com/twitter/storehaus/kafka/KafkaSinkSpec.scala
@@ -1,17 +1,17 @@
 /*
- * Copyright 2014 Twitter inc.
+ * Copyright 2014 Twitter Inc.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.twitter.storehaus.kafka
@@ -93,7 +93,7 @@ class KafkaSinkSpec extends WordSpec with Matchers with BeforeAndAfterAll {
 
       val sink = KafkaSink[String, String, StringSerializer, StringSerializer](
           topic, Seq(ktu.brokerAddress))
-        .filter { case (k , v) => v.toInt % 2 == 0 }
+        .filter { case (k, v) => v.toInt % 2 == 0 }
 
       val futures = (1 to 10)
         .map(i => ("key", i.toString))
