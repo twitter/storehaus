ffeba69889cb12fdb8846321e9398aeb58ca9d02
BenFradet <benjamin.fradet@gmail.com>
2016-06-26 11:00:35 +0200

    cleanup storehaus-http

--- storehaus-http/src/main/scala/com/twitter/storehaus/http/HttpStore.scala
+++ storehaus-http/src/main/scala/com/twitter/storehaus/http/HttpStore.scala
@@ -17,8 +17,9 @@
 package com.twitter.storehaus.http
 
 import java.nio.charset.Charset
-import org.jboss.netty.buffer.{ ChannelBuffer, ChannelBuffers }
-import org.jboss.netty.handler.codec.http.{ HttpRequest, HttpResponse, DefaultHttpRequest, HttpVersion, HttpMethod, HttpHeaders, HttpResponseStatus }
+import org.jboss.netty.buffer.ChannelBuffer
+import org.jboss.netty.handler.codec.http.{ HttpRequest, HttpResponse, DefaultHttpRequest,
+  HttpVersion, HttpMethod, HttpHeaders, HttpResponseStatus }
 import com.twitter.util.Future
 import com.twitter.bijection.StringCodec
 import com.twitter.bijection.netty.ChannelBufferBijection
@@ -27,21 +28,25 @@ import com.twitter.finagle.http.compat.NettyClientAdaptor
 import com.twitter.storehaus.{ Store, ConvertedStore }
 
 object HttpException {
-  def apply(response: HttpResponse): HttpException = 
-    new HttpException(response.getStatus.getCode, response.getStatus.getReasonPhrase, response.getContent.toString(Charset.forName("UTF-8")))
+  def apply(response: HttpResponse): HttpException =
+    new HttpException(response.getStatus.getCode, response.getStatus.getReasonPhrase,
+      response.getContent.toString(Charset.forName("UTF-8")))
 }
 
-case class HttpException(code: Int, reasonPhrase: String, content: String) extends Exception(reasonPhrase + Option(content).map("\n" + _ ).getOrElse(""))
+case class HttpException(code: Int, reasonPhrase: String, content: String)
+  extends Exception(reasonPhrase + Option(content).map("\n" + _ ).getOrElse(""))
 
 object HttpStore {
-  def apply(dest: String): HttpStore = new HttpStore(NettyClientAdaptor andThen Http.newService(dest))
+  def apply(dest: String): HttpStore =
+    new HttpStore(NettyClientAdaptor andThen Http.newService(dest))
 }
 
-class HttpStore(val client: Service[HttpRequest, HttpResponse]) extends Store[String, ChannelBuffer] {
+class HttpStore(val client: Service[HttpRequest, HttpResponse])
+    extends Store[String, ChannelBuffer] {
   override def get(k: String): Future[Option[ChannelBuffer]] = {
-    val request =  new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, k)
+    val request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, k)
     request.headers.set(HttpHeaders.Names.CONTENT_LENGTH, "0")
-    client(request).map{ response => 
+    client(request).map{ response =>
       response.getStatus match {
         case HttpResponseStatus.OK => Some(response.getContent)
         case HttpResponseStatus.NOT_FOUND => None
@@ -74,9 +79,10 @@ class HttpStore(val client: Service[HttpRequest, HttpResponse]) extends Store[St
 }
 
 object HttpStringStore {
-  def apply(dest: String): HttpStringStore = new HttpStringStore(NettyClientAdaptor andThen Http.newService(dest))
+  def apply(dest: String): HttpStringStore =
+    new HttpStringStore(NettyClientAdaptor andThen Http.newService(dest))
 }
 
 class HttpStringStore(val client: Service[HttpRequest, HttpResponse])
-    extends ConvertedStore[String, String, ChannelBuffer, String](new HttpStore(client))(identity)(
-      StringCodec.utf8 andThen ChannelBufferBijection.inverse)
+    extends ConvertedStore[String, String, ChannelBuffer, String](
+      new HttpStore(client))(identity)(StringCodec.utf8 andThen ChannelBufferBijection.inverse)
