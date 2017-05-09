0c739f434dfe3fc53dcac880e62ff74085fda57f
Pankaj Gupta <pankaj.roark@gmail.com>
2017-05-08 21:47:08 -0700

    Mongodb tests were going OOM due to creation of client numerous times, again due to property declaration behavior change in scalacheck. Fix that.

--- storehaus-mongodb/src/test/scala/com/twitter/storehaus/mongodb/MongoStoreProperties.scala
+++ storehaus-mongodb/src/test/scala/com/twitter/storehaus/mongodb/MongoStoreProperties.scala
@@ -31,6 +31,18 @@ import com.mongodb.casbah.Imports._
 
 object MongoStoreProperties extends Properties("MongoStore") {
 
+  private[this] class PropertyCached(ps: PropertySpecifier) {
+    def update(propName: String, p: Prop) = {
+      ps(propName) = p
+    }
+  }
+
+  /**
+    * Property specification is used by name since scalacheck 1.13.4, which breaks
+    * tests here. This simulates the old behavior.
+    */
+  private[this] val propertyCached = new PropertyCached(property)
+
   def putAndGetStoreTest[K, V](store: Store[K, V], pairs: Gen[List[(K, Option[V])]]): Prop =
     forAll(pairs) {
       (examples: List[(K, Option[V])]) => {
@@ -43,49 +55,49 @@ object MongoStoreProperties extends Properties("MongoStore") {
       }
     }
 
-  property("MongoStore[String, String]") =
+  propertyCached("MongoStore[String, String]") =
     putAndGetStoreTest[String, String](MongoStore[String, String](
         MongoClient("127.0.0.1", 27017),
         "storehaus",
         "data.stringMap"
       ), NonEmpty.Pairing.alphaStrs())
 
-  property("MongoStore[Long, Long]") =
+  propertyCached("MongoStore[Long, Long]") =
     putAndGetStoreTest[Long, Long](MongoStore[Long, Long](
         MongoClient("127.0.0.1", 27017),
         "storehaus",
         "data.longMap"
       ), NonEmpty.Pairing.numerics[Long]())
 
-  property("MongoStore[Int, Int]") =
+  propertyCached("MongoStore[Int, Int]") =
     putAndGetStoreTest[Int, Int](MongoStore[Int, Int](
         MongoClient("127.0.0.1", 27017),
         "storehaus",
         "data.intMap"
       ), NonEmpty.Pairing.numerics[Int]())
 
-  property("MongoStore[Double, Double]") =
+  propertyCached("MongoStore[Double, Double]") =
     putAndGetStoreTest[Double, Double](MongoStore[Double, Double](
         MongoClient("127.0.0.1", 27017),
         "storehaus",
         "data.doubleMap"
       ), NonEmpty.Pairing.numerics[Double]())
 
-  property("MongoStore[String, Int]") =
+  propertyCached("MongoStore[String, Int]") =
     putAndGetStoreTest[String, Int](MongoStore[String, Int](
         MongoClient("127.0.0.1", 27017),
         "storehaus",
         "data.stringIntMap"
       ), NonEmpty.Pairing.alphaStrNumerics[Int]())
 
-  property("MongoStore[String, Long]") =
+  propertyCached("MongoStore[String, Long]") =
     putAndGetStoreTest[String, Long](MongoStore[String, Long](
         MongoClient("127.0.0.1", 27017),
         "storehaus",
         "data.stringLongMap"
       ), NonEmpty.Pairing.alphaStrNumerics[Long]())
 
-  property("MongoStore[String, Double]") =
+  propertyCached("MongoStore[String, Double]") =
     putAndGetStoreTest[String, Double](MongoStore[String, Double](
         MongoClient("127.0.0.1", 27017),
         "storehaus",
